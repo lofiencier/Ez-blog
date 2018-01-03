40e9b5eafae4591efffdfad010a2876eb3a2426b
[E.N.V.Y] <842028554@qq.com>
2018-01-04 00:26:13 +0800

    env create

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  body,document{
+    padding:0;
+    margin:0;
+  }
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="what">
+     缩进有问题也要报错？
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
